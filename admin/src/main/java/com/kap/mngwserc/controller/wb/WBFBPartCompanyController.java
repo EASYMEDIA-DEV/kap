5f35e3d0f96f8788f7d0a73dc1681583c5601be1
dh_kim <dh_kim@easymedia.net>
2023-11-16 16:20:27 +0900

    Changes

--- admin/src/main/java/com/kap/mngwserc/controller/wb/WBFBPartCompanyController.java
+++ admin/src/main/java/com/kap/mngwserc/controller/wb/WBFBPartCompanyController.java
@@ -1,16 +1,10 @@
 package com.kap.mngwserc.controller.wb;
 
-import com.kap.core.dto.wb.wbf.WBFASmartRoundDTO;
-import com.kap.service.COCodeService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.ArrayList;
 
 /**
  * <pre>
