5648be9058c88376351ee4d4fff0a8d9e30ce0f3
yooho30 <yooho30@easymedia.net>
2023-11-20 10:57:30 +0900

    교육차수관리 레이어팝업 추가

--- admin/src/main/webapp/common/js/controller/co/COCmmCtrl.js
+++ admin/src/main/webapp/common/js/controller/co/COCmmCtrl.js
@@ -1624,12 +1624,27 @@ var cmmCtrl = (function(){
 	}
 
 	//교육과정 검색 매핑
-	//getPartsCompanyLayerPop(function(data){data 객체를 받아서 처리});
+	//getCouseSrchLayerPop(function(data){data 객체를 받아서 처리});
 	var fn_couse_srch_layer_pop = function(fnc){
 		$(".ebbEpisdCouseSrchLayer").one('show.bs.modal', function() {
 			// Add class for soft backdrop
 			$(".ebbEpisdCouseSrchLayer").find("#btnRefresh").click();
 			$(".ebbEpisdCouseSrchLayer").find("#btnSearch").click();
+		}).one('hidden.bs.modal', function() {
+			// Remove class for soft backdrop (if not will affect future modals)
+		}).one('choice', function(data, param) {
+			// Remove class for soft backdrop (if not will affect future modals)
+			fnc(param);
+		}).modal();
+	}
+
+	//교육차수 검색 매핑
+	//getEpisdSrchLayerPop(function(data){data 객체를 받아서 처리});
+	var fn_episd_srch_layer_pop = function(fnc){
+		$(".ebbEpisdMstSrchLayer").one('show.bs.modal', function() {
+			// Add class for soft backdrop
+			$(".ebbEpisdMstSrchLayer").find("#btnRefresh").click();
+			$(".ebbEpisdMstSrchLayer").find("#btnSearch").click();
 		}).one('hidden.bs.modal', function() {
 			// Remove class for soft backdrop (if not will affect future modals)
 		}).one('choice', function(data, param) {
@@ -1652,6 +1667,7 @@ var cmmCtrl = (function(){
 			fnc(param);
 		}).modal();
 	}
+
 
 	return {
 		nvl : fn_replace_null,
@@ -1695,6 +1711,8 @@ var cmmCtrl = (function(){
 		getLecturerLayerPop : fn_lec_layer_pop,
 		//교육과정검색 매핑
 		getCouseSrchLayerPop : fn_couse_srch_layer_pop,
+		//교육차수검색 매핑
+		getEpisdSrchLayerPop : fn_episd_srch_layer_pop,
 		//일반회원 매핑
 		getUserSrchPop:fn_user_srch_layer_pop
 	}
