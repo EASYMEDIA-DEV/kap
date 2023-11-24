c679caac81472545427110cd2248c22e8c536738
jds1004 <jds7447@easymedia.net>
2023-11-24 18:18:02 +0900

    교육장 관리 수정

--- admin/src/main/java/com/kap/mngwserc/controller/eb/EBFEduRoomController.java
+++ admin/src/main/java/com/kap/mngwserc/controller/eb/EBFEduRoomController.java
@@ -9,10 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.ArrayList;
