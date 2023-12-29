7b67e282a4c7d40fdc5e1ab13522f3c9aea68120
yungi <yungi@easymedia.net>
2023-12-29 17:48:20 +0900

    미래차 공모전 수정

--- admin/src/main/webapp/common/js/controller/wb/wbk/WBKPlaceMakeCtrl.js
+++ admin/src/main/webapp/common/js/controller/wb/wbk/WBKPlaceMakeCtrl.js
@@ -11,6 +11,44 @@ define(["ezCtrl", "ezVald", "CodeMirror", "CodeMirror.modeJs"], function(ezCtrl,
     var ctrl = new ezCtrl.controller(exports.controller);
     ctrl.model = {
         id : {
+            room :{
+                event :{
+                    click: function(){
+                        console.log(1);
+                        cmmCtrl.getEduRoomLayerPop(function(data){
+                            console.log(data);
+
+                            var seq = data.seq
+                            var titl = data.titl // 장소명
+                            var rgnsNm = data.rgnsNm // 지역
+                            var bscAddr = data.addr; // 주소
+                            var rprsntTelNo = data.rprsntTelNo; // 대표번호
+
+                            $("input[name=placeSeq]").val(seq);
+
+                            var tc = new Array(); // 배열 생성
+                            tc.push({titl : titl, rgnsName : rgnsNm, bscAddr : bscAddr ,rprsntTelNo : rprsntTelNo }); // 배열 값 넣기
+
+                            // 해당 지역 선택 시 테이블 td 그리기
+                            var html = '';
+
+                            for(var i = 0; i < 1; i++){
+                                html += '<tr>';
+                                html += '<td class=\"text-center\">'+tc[i].titl+'</td>';
+                                html += '<td class=\"text-center\">'+tc[i].rgnsName+'</td>';
+                                html += '<td class=\"text-center\">'+tc[i].bscAddr+'</td>';
+                                html += '<td class=\"text-center\">'+tc[i].rprsntTelNo+'</td>';
+                                html += '</tr>';
+                            }
+
+                            $("#listContainer").empty();
+                            $("#listContainer").append(html);
+                        });
+
+                    }
+                }
+            },
+
         },
         classname : {
             btnEduPlaceLayerChoice :{
