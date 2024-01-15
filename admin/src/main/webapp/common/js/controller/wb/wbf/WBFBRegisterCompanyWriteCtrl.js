93b1314a8734292475b6037280108462fc33cf56
dh_kim <dh_kim@easymedia.net>
2024-01-15 18:14:52 +0900

    20240115 - 상생 스마트공장구축 수정사항

--- admin/src/main/webapp/common/js/controller/wb/wbf/WBFBRegisterCompanyWriteCtrl.js
+++ admin/src/main/webapp/common/js/controller/wb/wbf/WBFBRegisterCompanyWriteCtrl.js
@@ -22,7 +22,7 @@ define(["ezCtrl","ezVald", "CodeMirror", "CodeMirror.modeJs"], function(ezCtrl, 
 
         /* 사업자번호 변경 */
         let dataBsnmNo = rtnData['bsnmNo'];
-        rtnData['bsnmNo'] = dataBsnmNo.slice(0,3) + '-' + dataBsnmNo.slice(3,5) + '-' + dataBsnmNo.slice(5);
+        rtnData['changeBsnmNo'] = dataBsnmNo.slice(0,3) + '-' + dataBsnmNo.slice(3,5) + '-' + dataBsnmNo.slice(5);
 
         rtnData['nameAndId'] = `${rtnData['name']}(${rtnData['id']})`;
 
