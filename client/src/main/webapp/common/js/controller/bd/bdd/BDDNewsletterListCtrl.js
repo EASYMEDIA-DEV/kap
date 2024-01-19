dcb7f23d2be64c0205ec0f1af45ebd704e8d71fc
eghee <eghee@easymedia.net>
2024-01-19 18:11:03 +0900

    사용자 뉴스레터 구독 수정

--- client/src/main/webapp/common/js/controller/bd/bdd/BDDNewsletterListCtrl.js
+++ client/src/main/webapp/common/js/controller/bd/bdd/BDDNewsletterListCtrl.js
@@ -9,7 +9,9 @@ define(["ezCtrl", "ezVald"], function(ezCtrl, ezVald) {
 
     // get controller object
     var ctrl = new ezCtrl.controller(exports.controller);
-
+    var chilCnt = $("#infoCard").children("a").length; // 게시물 수
+    var pageCnt = 1; // 페이지 카운트
+    var page = (chilCnt / 9); // 더보기 페이지
     // form Object
     var $formObj = ctrl.obj.find("form").eq(0);
 
@@ -35,18 +37,6 @@ define(["ezCtrl", "ezVald"], function(ezCtrl, ezVald) {
                 }, "/foundation/board/newsletter/insert",  newsletterForm, "text");
             }
         }
-    }
-
-    //목록 조회
-    var search = function(page){
-        if(page != undefined){
-            $formObj.find("#pageIndex").val(page);
-        }
-        cmmCtrl.listFrmAjax(function(respObj) {
-            ctrl.obj.find(".board-list").html(respObj);
-            var totCnt = $(respObj).eq(0).data("totalCount");
-            ctrl.obj.find("#listContainerTotCnt").text(totCnt);
-        }, "/foundation/board/newsletter/select", $formObj, "GET", "html");
     }
 
     // set model
@@ -75,39 +65,31 @@ define(["ezCtrl", "ezVald"], function(ezCtrl, ezVald) {
                         $("#subscribe").addClass("subscribeBtn");
                     }
                 }
+            },
+            searchBtn : {
+                event : {
+                    click : function() {
+                        location.href = "./list?srchVal=" + $("#srchVal").val();
+                    }
+                }
             }
         },
         classname : {
-            //페이징 처리
-            pageSet : {
+            moreBtn : {
                 event : {
-                    click : function() {
-                        //페이징 이동
-                        if( $(this).attr("value") != "null" ){
-                            $formObj.find("input[name=pageIndex]").val($(this).attr("value"));
-                            search();
+                    click : function(){
+                        pageCnt = pageCnt + 1; // 더보기 누를 때마다 1씩 증가
+                        var openCnt = $("#infoCard").find(".open").length // 보이는 게시물
+                        var closeCnt = $("#infoCard").find(".close").length; // 숨겨진 게시물
+                        if(pageCnt <= page){
+                            $("#infoCard").children("a").slice(openCnt+1,openCnt+10).show();
+                            $("#infoCard").children("a").slice(openCnt+1,openCnt+10).removeClass("open");
+                            $("#infoCard").children("a").slice(openCnt+1,openCnt+10).addClass("open");
+                            $(".cntText").text(openCnt+9 +"/"+ chilCnt);
+                        }else{
+                            $("#infoCard").find(".close").show();
+                            $(".moreBtn").hide();
                         }
-                    }
-                }
-            },
-            //상세보기
-            listView : {
-                event : {
-                    click : function() {
-                        //상세보기
-                        var detailsKey = $(this).data("detailsKey");
-                        $formObj.find("input[name=detailsKey]").val(detailsKey);
-                        location.href = "./write?" + $formObj.serialize();
-                    }
-                }
-            },
-            //페이징 목록 갯수
-            listRowSizeContainer : {
-                event : {
-                    change : function(){
-                        //리스트 갯수 변경
-                        $formObj.find("input[name=listRowSize]").val($(this).val());
-                        search(1);
                     }
                 }
             },
@@ -147,12 +129,28 @@ define(["ezCtrl", "ezVald"], function(ezCtrl, ezVald) {
                         }
                     }
                 }
+            },
+            //상세보기
+            listView : {
+                event : {
+                    click : function() {
+                        //상세보기
+                        location.href = "./view?" + $formObj.serialize();
+                    }
+                }
             }
         },
         immediately : function() {
-            //리스트 조회
-            search();
-
+            if(chilCnt > 9){
+                $("#infoCard").children("a").slice(9,chilCnt).hide();
+                $("#infoCard").children("a").slice(9,chilCnt).removeClass("open");
+                $("#infoCard").children("a").slice(9,chilCnt).addClass("close");
+                var openCnt = $("#infoCard").find(".open").length // 보이는 게시물
+                var closeCnt = $("#infoCard").find(".close").length; // 숨겨진 게시물
+                $(".cntText").text(openCnt +"/"+ chilCnt);
+            }else{
+                $(".moreBtn").hide();
+            }
         }
     };
 
