61967b387b134dd9d887e1c0fe11276e7ddf1ebf
daesung <daesung@easymedia.net>
2024-01-31 11:01:46 +0900

    상생사업관리 수정

--- client/src/main/java/com/kap/front/controller/wb/wbc/WBCSecurityController.java
+++ client/src/main/java/com/kap/front/controller/wb/wbc/WBCSecurityController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
@@ -258,4 +259,45 @@ public class WBCSecurityController {
         return vwUrl;
     }
 
+    /**
+     * 등록 사업자번호 매핑 여부 확인
+     */
+    @PostMapping(value="/getInsertBsnmNoCnt")
+    public String getInsertBsnmNoCnt(WBCBSecurityMstInsertDTO wBCBSecurityMstInsertDTO, ModelMap modelMap, HttpServletRequest request) throws Exception
+    {
+        try
+        {
+            modelMap.addAttribute("respCnt", wBCBSecurityService.getInsertBsnmNoCnt(wBCBSecurityMstInsertDTO));
+        }
+        catch (Exception e)
+        {
+            if (log.isDebugEnabled())
+            {
+                log.debug(e.getMessage());
+            }
+            throw new Exception(e.getMessage());
+        }
+        return "jsonView";
+    }
+
+    /**
+     * 등록 종된 사업자번호 매핑 여부 확인
+     */
+    @PostMapping(value="/getInsertSbrdnBsnmNoCnt")
+    public String getInsertSbrdnBsnmNoCnt(WBCBSecurityMstInsertDTO wBCBSecurityMstInsertDTO, ModelMap modelMap, HttpServletRequest request) throws Exception
+    {
+        try
+        {
+            modelMap.addAttribute("respCnt", wBCBSecurityService.getInsertSbrdnBsnmNoCnt(wBCBSecurityMstInsertDTO));
+        }
+        catch (Exception e)
+        {
+            if (log.isDebugEnabled())
+            {
+                log.debug(e.getMessage());
+            }
+            throw new Exception(e.getMessage());
+        }
+        return "jsonView";
+    }
 }
