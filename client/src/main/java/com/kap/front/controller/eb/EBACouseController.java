70b7ad0abab605e97bf37002d5ad4d54c3118933
yooho30 <yooho30@easymedia.net>
2024-03-08 10:26:33 +0900

    사용자 전체 레이어 팝업 출력조건 수정 관리자 교육차수관리 출력조건 수정

--- client/src/main/java/com/kap/front/controller/eb/EBACouseController.java
+++ client/src/main/java/com/kap/front/controller/eb/EBACouseController.java
@@ -624,6 +624,7 @@ public class EBACouseController {
             EBBEpisdDTO rtnMap = new EBBEpisdDTO();
             try
             {
+                eBBEpisdDTO.setApplyListYn("N");
                 rtnMap = eBBEpisdService.selectEpisdList(eBBEpisdDTO);
             }
             catch (Exception e)
